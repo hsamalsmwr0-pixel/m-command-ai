637d13287f9d2be1c8c55e2123940947375158ae
hsamalsmwr0-pixel <hsamalsmwr0@gmail.com>
2026-09-06 16:29:23 +0300

    Add Projects component with Arabic content

---
+++ src/Projects.tsx
@@ -0,0 +1,9 @@
+export default function Projects() {
+  return (
+    <section className="page">
+      <h1>المشاريع</h1>
+      <p>إدارة مشاريعك ومتابعة مراحل التنفيذ.</p>
+    </section>
+  );
+}
+
